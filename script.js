a64a78a473abe01fd518e544fdbed1d407ff7709
Deivid021 <deividsestrensantos@gmail.com>
2024-10-22 19:41:12 -0300

    criada uma funcao para o js de rolagem

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+function setupScrollButton() {
+    document.getElementById("scrollButton").addEventListener("click", function() {
+        const scrollTarget = window.innerHeight; // Rola 100% da altura da tela
+        window.scrollTo({
+            top: scrollTarget,
+            behavior: "smooth" 
+        });
+    });
+}
+
+// Chama a função para configurar o botão após o carregamento da página
+window.onload = setupScrollButton;
